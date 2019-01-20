c0bbee28be1bb752959e225fb516fcb541b01eea
echavez85 <echavez85@gmail.com>
2019-01-19 21:44:37 -0600

    added dummy individual to friends js.

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+{
+    "name":"Eddie",
+    "photo":"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/22555173_10100313095721064_8812933739482121158_n.jpg?_nc_cat=109&_nc_ht=scontent-ort2-2.xx&oh=5f41cfe35fcbf015bb16ce79fd404fe6&oe=5CBA804E",
+    "scores":[
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        1
+      ]
+  }
+  
