08768f1e3d510f9a5ef9bad56d5c0127feae87e7
echavez85 <echavez85@gmail.com>
2019-01-19 21:56:10 -0600

    added var to friends js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-{
+var friends = [{
     "name":"Eddie",
     "photo":"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/22555173_10100313095721064_8812933739482121158_n.jpg?_nc_cat=109&_nc_ht=scontent-ort2-2.xx&oh=5f41cfe35fcbf015bb16ce79fd404fe6&oe=5CBA804E",
     "scores":[
@@ -13,5 +13,6 @@
         5,
         1
       ]
-  }
+  },
+]
   
